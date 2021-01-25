f158eb21f132d0b33e11a279f0cc90b01b3e3904
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-01-25 17:44:38 +0000

    export to asset Fix #1

---
+++ component/parameter/.ipynb_checkpoints/values-checkpoint.py
@@ -0,0 +1,5 @@
+# minimum year for the alos images 
+min_year = 2015
+
+# maximum year for the alos images 
+max_year = 2018
---
+++ component/parameter/.ipynb_checkpoints/viz-checkpoint.py
@@ -0,0 +1,7 @@
+VisParam   = {
+    "opacity": 1,
+    "bands": ["HH","HV","HH"],
+    "min": 160.36,
+    "max": 7857.64,
+    "gamma": 1
+}
