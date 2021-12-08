6e2c1ff57e9642573b292b934f76e798882888bb
12rambau <pierrick.rambaud49@gmail.com>
2021-12-08 14:01:02 +0000

    apply linter

--- component/scripts/display.py
+++ component/scripts/display.py
@@ -8,28 +8,29 @@ from component import parameter as pm
 
 ee.Initialize()
 
+
 def display_result(ee_aoi, dataset, m, vis):
     """
-    Display the results on the map 
-    
+    Display the results on the map
+
     Args:
         ee_aoi: (ee.Geometry): the geometry of the aoi
         dataset (ee.Image): the image the display
         m (sw.SepalMap): the map used for the display
         db (bool): either to use the db scale or not
-        
+
     Return:
         (sw.SepalMap): the map with the different layers added
     """
-    # AOI borders in blue 
-    empty   = ee.Image().byte()
-    outline = empty.paint(featureCollection = ee_aoi, color = 1, width = 3)
-   
+    # AOI borders in blue
+    empty = ee.Image().byte()
+    outline = empty.paint(featureCollection=ee_aoi, color=1, width=3)
+
     # Zoom to AOI
     m.zoom_ee_object(ee_aoi.geometry())
-    
+
     # Add objects
-    m.addLayer(outline, {'palette': color.info}, 'aoi')
-    m.addLayer(dataset, vis, ms.process.alos_mosaic) 
-    
+    m.addLayer(outline, {"palette": color.info}, "aoi")
+    m.addLayer(dataset, vis, ms.process.alos_mosaic)
+
     return
--- component/tile/process.py
+++ component/tile/process.py
@@ -1,4 +1,4 @@
-# It is strongly suggested to use a separate file to define the tiles of your process and then call them in your notebooks. 
+# It is strongly suggested to use a separate file to define the tiles of your process and then call them in your notebooks.
 # it will help you to have control over their fonctionalities using object oriented programming
 
 from sepal_ui import sepalwidgets as sw
@@ -10,85 +10,72 @@ from component import scripts
 from component.message import ms
 from component import parameter as pm
 
-# the tiles should all be heriting from the sepal_ui Tile object 
-# if you want to create extra reusable object, you can define them in an extra widget.py file 
+# the tiles should all be heriting from the sepal_ui Tile object
+# if you want to create extra reusable object, you can define them in an extra widget.py file
 class ProcessTile(sw.Tile):
-    
     def __init__(self, model, aoi_model, viz_tile, export_tile, **kwargs):
-        
+
         # Define the model and the aoi_model as class attribute so that they can be manipulated in its custom methods
-        self.model = model 
+        self.model = model
         self.aoi_model = aoi_model
-        
-        # Link to the result tile 
+
+        # Link to the result tile
         self.viz_tile = viz_tile
         self.export_tile = export_tile
-        
+
         # widgets
         self.year = v.Select(
-            label = ms.process.slider,
-            v_model = None,
-            items = pm.years[::-1]
+            label=ms.process.slider, v_model=None, items=pm.years[::-1]
         )
-        
+
         self.filter = v.Select(
-            label = ms.process.filter,
-            v_model = None,
-            items = pm.speckle_filters            
+            label=ms.process.filter, v_model=None, items=pm.speckle_filters
         )
-        
-        self.ls_mask = v.Switch(
-            class_ = "ml-5",
-            label = ms.process.ls_mask,
-            v_model = True
-        )
-        
-        self.dB = v.Switch(
-            class_ = "ml-5",
-            label = ms.process.dB,
-            v_model = True
-        )
-         
+
+        self.ls_mask = v.Switch(class_="ml-5", label=ms.process.ls_mask, v_model=True)
+
+        self.dB = v.Switch(class_="ml-5", label=ms.process.dB, v_model=True)
+
         # it also has the embeded `bind` method that link mutable variable to component v_model
-        # bind return self so it can be chained to bind everything in one statement. 
+        # bind return self so it can be chained to bind everything in one statement.
         # args are (widget, model, model_attribute_name)
-        self.model \
-            .bind(self.year, 'year')  \
-            .bind(self.filter, 'filter') \
-            .bind(self.ls_mask, 'ls_mask') \
-            .bind(self.dB, 'dB')
-        
-        # construct the Tile with the widget we have initialized 
+        self.model.bind(self.year, "year").bind(self.filter, "filter").bind(
+            self.ls_mask, "ls_mask"
+        ).bind(self.dB, "dB")
+
+        # construct the Tile with the widget we have initialized
         super().__init__(
-            id_ = "process_widget", # the id will be used to make the Tile appear and disapear
-            title  = ms.process.title, # the Title will be displayed on the top of the tile
-            inputs = [self.year, self.filter, self.ls_mask, self.dB],#self.asset,
-            btn = sw.Btn(ms.process.process),
-            alert = sw.Alert()
+            id_="process_widget",  # the id will be used to make the Tile appear and disapear
+            title=ms.process.title,  # the Title will be displayed on the top of the tile
+            inputs=[self.year, self.filter, self.ls_mask, self.dB],  # self.asset,
+            btn=sw.Btn(ms.process.process),
+            alert=sw.Alert(),
         )
-        
+
         # now that the Tile is created we can link it to a specific function
         self.btn.on_event("click", self._on_run)
-        
+
     @su.loading_button(debug=False)
-    def _on_run(self, widget, data, event): 
-            
+    def _on_run(self, widget, data, event):
+
         # check that the input that you're gonna use are set (Not mandatory)
-        if not self.alert.check_input(self.aoi_model.name, ms.process.no_aoi): return widget.toggle_loading()
-        if not self.alert.check_input(self.model.year, ms.process.no_slider): return widget.toggle_loading()
-        
+        if not self.alert.check_input(self.aoi_model.name, ms.process.no_aoi):
+            return widget.toggle_loading()
+        if not self.alert.check_input(self.model.year, ms.process.no_slider):
+            return widget.toggle_loading()
+
         # enable or diasble the use of fnf
         no_fnf = self.model.year >= pm.last_fnf_year
-        
-        # change the radio btn for viz 
+
+        # change the radio btn for viz
         # default to backscatter value
         self.viz_tile.visSelect.v_model = self.viz_tile.visSelect.children[0].value
         self.viz_tile.visSelect.children[-1].disabled = no_fnf
-        
+
         # change switch value in export
         self.export_tile.fnf.disabled = no_fnf
         self.export_tile.fnf.v_model = min(not no_fnf, self.export_tile.fnf.v_model)
-            
+
         # Create the mosaic
         dataset = scripts.create(
             self.aoi_model.feature_collection,
@@ -96,10 +83,10 @@ class ProcessTile(sw.Tile):
             self.alert,
             speckle_filter=self.model.filter,
             ls_mask=self.model.ls_mask,
-            db=self.model.dB
+            db=self.model.dB,
         )
 
-        # change the model values as its a mutable object 
+        # change the model values as its a mutable object
         # useful if the model is used as an input in another tile
         self.model.dataset = dataset
 
@@ -108,11 +95,9 @@ class ProcessTile(sw.Tile):
         self.export_tile.sepal_btn.disabled = False
 
         # conclude the computation with a message
-        self.alert.add_live_msg(ms.process.end_computation, 'success')
+        self.alert.add_live_msg(ms.process.end_computation, "success")
 
         # launch vizualisation
         self.viz_tile._on_change(None)
-        
+
         return
-        
-        
